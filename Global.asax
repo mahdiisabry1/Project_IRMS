82a039ad558a32d7f74f1516619532d1dbc331c1
Wimukthi Manchanayaka <139443692+Wimukthi-Manchanayaka@users.noreply.github.com>
2024-12-02 09:17:17 +0530

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_IRMS.MvcApplication" Language="C#" %>
---
+++ Project_IRMS.Buisness/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_IRMS.Buisness
+{
+    public class Class1
+    {
+    }
+}
