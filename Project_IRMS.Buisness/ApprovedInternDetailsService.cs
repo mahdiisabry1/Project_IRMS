c26a68ca0dcc2beef264b3c397057a232f424550
Wimukthi Manchanayaka <wimukthimanchanayakawork@gmail.com>
2025-01-08 10:45:04 +0530

    approved requests

---
+++ Project_IRMS.Buisness/ApprovedInternDetailsService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_IRMS.Models;
+using System.Data;
+using Project_IRMS.Data;
+
+namespace Project_IRMS.Buisness
+{
+    public class ApprovedInternDetailsService
+    {
+        private readonly ApprovedInternDetails _dataAccess;
+
+        public ApprovedInternDetailsService()
+        {
+            _dataAccess = new ApprovedInternDetails(); //connecting businesslayer with datalayer 
+        }
+        public List<Intern> GetAllInterns(string tablename)
+        {
+            var internList = new List<Intern>();
+            DataTable dataTable = _dataAccess.GetInternDetails(tablename);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                internList.Add(new Intern
+                {
+                    InternID = Convert.ToInt32(row["InternID"]),
+                    FirstName = row["FirstName"].ToString(),
+                    LastName = row["LastName"].ToString(),
+                    University = row["University"].ToString(),
+                    Gender = row["Gender"].ToString(),
+                    Email = row["Email"].ToString(),
+                    ContactNo = row["ContactNo"].ToString(),
+                    Degree = row["Degree"].ToString(),
+                    Division = row["Division"].ToString(),
+                    ProfileImage = row.IsNull("ProfileImage") ? null : (byte[])row["ProfileImage"],
+                    CV = row.IsNull("CV") ? null : (byte[])row["CV"]
+                });
+            }
+
+            return internList;
+        }
+    }
+}
---
+++ Project_IRMS.Data/ApprovedInternDetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Project_IRMS.Data.Common;
+using System.Data.SqlClient;
+
+namespace Project_IRMS.Data
+{
+    public class ApprovedInternDetails
+    {
+        private readonly SqlServerHelper _sqlHelper;
+
+        public ApprovedInternDetails()
+        {
+            _sqlHelper = new SqlServerHelper();
+        }
+        // Method to validate table name against allowed list
+        private void ValidateTableName(string tableName)
+        {
+            string[] allowedTables = { "InternDetails", "hr", "it" }; // Add your valid table names here
+            if (!allowedTables.Contains(tableName))
+                throw new ArgumentException($"Invalid table name: {tableName}");
+        }
+
+        public DataTable GetInternDetails(string tablename)
+        {
+            ValidateTableName(tablename); // Ensure table name is valid
+
+            string query = $@"SELECT * FROM {tablename} WHERE Status = @Status";
+
+            SqlParameter[] parameters = {
+                new SqlParameter("@Status", "approve")
+            };
+
+            return _sqlHelper.ExecuteQuery(query, CommandType.Text, parameters);
+        }
+    }
+}
